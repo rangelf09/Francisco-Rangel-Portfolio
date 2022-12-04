f73be5247bb2d38a26b8ac378a7c6708ea7eb0d9
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-04 16:08:53 -0600

    Updates

---
+++ precache-manifest.8da3becdff09c67eeaa14ca58be41ba2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e58406529b241cb7830b283194e32f8",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/index.html"
+  },
+  {
+    "revision": "e130df8f1d83abe1fb9a",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "6ec5eb60f2721f312a21",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/static/css/main.1341aae1.chunk.css"
+  },
+  {
+    "revision": "e130df8f1d83abe1fb9a",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/static/js/2.932870ba.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/static/js/2.932870ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec5eb60f2721f312a21",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/static/js/main.49d22c54.chunk.js"
+  },
+  {
+    "revision": "b000bef33c7c23dcd5c5",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/static/js/runtime-main.689e124e.js"
+  },
+  {
+    "revision": "e259280dda247059a94a5a39966cad8b",
+    "url": "/rangelf09/Francisco-Rangel-Portfolio/static/media/Self.e259280d.png"
+  }
+]);
