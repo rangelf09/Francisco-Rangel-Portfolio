09e6f18bdac20a60a8841b74778b369e079e49f2
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-04 00:52:34 -0600

    added the home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeOutline from "../HomeOutline"
+
+function Home() {
+    return (
+        <div>
+            <HomeOutline />
+        </div>
+    );
+}
+
+export default Home;
