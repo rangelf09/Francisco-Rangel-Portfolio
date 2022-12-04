9ac34bdff386f96ec3a8b1df5574029825662851
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-12-04 00:38:33 -0600

    added the about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutOutline from "../AboutOutline"
+
+function About() {
+    return (
+        <div>
+            <AboutOutline />
+        </div>
+    );
+}
+
+export default AboutOutline;
